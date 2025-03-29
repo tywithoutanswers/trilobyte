5c49e21736c0a498971a927042cfb30e4a6a231c
Matthewhowley <mhowleyno1@gmail.com>
2025-03-29 21:58:45 +0000

    Add files via upload
    Main files i have been working on if you're curious . I'll merge them with the others and upload later

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    <h1 style="text-align: center; color: #2c3e50;">Trilobyte Trawling</h1>
+    <router-view></router-view>
+    <UploadJSON />
+  </div>
+</template>
+
+<script>
+import UploadJSON from './components/UploadJSON.vue';
+
+export default {
+  components: { UploadJSON },
+};
+</script>
